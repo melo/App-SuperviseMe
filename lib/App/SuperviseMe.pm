53514506fd099413291b92db6e4c51cd45ad2ce7
Pedro Melo <melo@simplicidade.org>
2013-01-25 11:31:13 +0000

    Build results of d7ce49e (on master)

--- lib/App/SuperviseMe.pm
+++ lib/App/SuperviseMe.pm
@@ -1,7 +1,4 @@
 package App::SuperviseMe;
-BEGIN {
-  $App::SuperviseMe::VERSION = '0.002';
-}
 
 # ABSTRACT: very simple command superviser
 
@@ -149,9 +146,14 @@ sub _error {
 
 1;
 
-
+__END__
 
 =pod
+
+=encoding utf-8
+
+=for :stopwords Pedro Melo ACKNOWLEDGEMENTS cpan testmatrix url annocpan anno bugtracker rt
+cpants kwalitee diff irc mailto metadata placeholders metacpan
 
 =head1 NAME
 
@@ -223,6 +225,81 @@ or a SIGTERM.
 
 L<AnyEvent>
 
+=head1 SUPPORT
+
+=head2 Perldoc
+
+You can find documentation for this module with the perldoc command.
+
+  perldoc App::SuperviseMe
+
+=head2 Websites
+
+The following websites have more information about this module, and may be of help to you. As always,
+in addition to those websites please use your favorite search engine to discover more resources.
+
+=over 4
+
+=item *
+
+MetaCPAN
+
+A modern, open-source CPAN search engine, useful to view POD in HTML format.
+
+L<http://metacpan.org/release/App-SuperviseMe>
+
+=item *
+
+CPAN Testers
+
+The CPAN Testers is a network of smokers who run automated tests on uploaded CPAN distributions.
+
+L<http://www.cpantesters.org/distro/A/App-SuperviseMe>
+
+=item *
+
+CPAN Testers Matrix
+
+The CPAN Testers Matrix is a website that provides a visual overview of the test results for a distribution on various Perls/platforms.
+
+L<http://matrix.cpantesters.org/?dist=App-SuperviseMe>
+
+=item *
+
+CPAN Testers Dependencies
+
+The CPAN Testers Dependencies is a website that shows a chart of the test results of all dependencies for a distribution.
+
+L<http://deps.cpantesters.org/?module=App::SuperviseMe>
+
+=item *
+
+CPAN Ratings
+
+The CPAN Ratings is a website that allows community ratings and reviews of Perl modules.
+
+L<http://cpanratings.perl.org/d/App-SuperviseMe>
+
+=back
+
+=head2 Email
+
+You can email the author of this module at C<MELO at cpan.org> asking for help with any problems you have.
+
+=head2 Bugs / Feature Requests
+
+Please report any bugs or feature requests through the web interface at L<https://github.com/melo/App-SuperviseMe/issues>. You will be automatically notified of any progress on the request by the system.
+
+=head2 Source Code
+
+The code is open to the world, and available for you to hack on. Please feel free to browse it and play
+with it, or whatever. If you want to contribute patches, please send me a diff or prod me to pull
+from your repository :)
+
+L<https://github.com/melo/App-SuperviseMe>
+
+  git clone git://github.com/melo/App-SuperviseMe.git
+
 =head1 AUTHOR
 
 Pedro Melo <melo@simplicidade.org>
@@ -233,10 +310,6 @@ This software is Copyright (c) 2011 by Pedro Melo.
 
 This is free software, licensed under:
 
-  The Artistic License 2.0
+  The Artistic License 2.0 (GPL Compatible)
 
 =cut
-
-
-__END__
-
